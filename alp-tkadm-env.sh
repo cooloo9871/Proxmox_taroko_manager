0132eccf7edab018570d2f055d86a15965548965
Andy <62133915+cooloo9871@users.noreply.github.com>
2024-06-13 20:37:07 +0800

    Update alp-tkadm-env.sh

--- alp-tkadm-env.sh
+++ alp-tkadm-env.sh
@@ -12,9 +12,6 @@ sudo rc-service cgroups start
 
 sudo ln -s /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64 && \
-chmod +x ./kind && \
-sudo mv ./kind /usr/local/bin/kind && \
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && \
 sudo rm -r kubectl
