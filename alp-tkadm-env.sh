fa6aa90fdd674e23f06dfe38d41a5611d6b285dd
Andy <62133915+cooloo9871@users.noreply.github.com>
2024-06-14 14:38:36 +0800

    Update alp-tkadm-env.sh

--- alp-tkadm-env.sh
+++ alp-tkadm-env.sh
@@ -19,12 +19,6 @@ sudo rm -r kubectl
 sudo curl https://dl.min.io/client/mc/release/linux-amd64/mc -o /usr/bin/mc
 sudo chmod +x /usr/bin/mc
 
-wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id=18F89F-SDhEe0lz29zvSxnK4KMQpr-9Jd' -O- | sed -rn 's/.*name="uuid" value=\"([0-9A-Za-z_\-]+).*/\1/p' > google_uuid.txt
-
-wget --load-cookies cookies.txt -O wulin.k1.tar.gz2 'https://drive.usercontent.google.com/download?export=download&id=18F89F-SDhEe0lz29zvSxnK4KMQpr-9Jd&confirm=t&uuid='$(<google_uuid.txt)
-
-tar -jxvf wulin.k1.tar.gz2
-
 sudo rm -r cookies.txt google_uuid.txt wulin.k1.tar.gz2
 
 cat <<EOF | sudo tee /etc/profile
